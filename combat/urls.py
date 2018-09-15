faf154fd184cae09f49b0c35668f0227eaad4d04
Keanu Hoang <hoang.keanu.k@gmail.com>
2018-09-15 04:34:17 +0000

    list of url paths on site

--- combat/urls.py
+++ combat/urls.py
@@ -6,4 +6,9 @@ app_name = 'combat'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('archive/', views.ArchiveView.as_view(), name='archive'),
+    path('new/', views.new, name='new'),
+    path('<int:character_id>/change/', views.change, name='change'),
+    path('add/', views.add, name='add'),
+    path('<int:character_id>/delete/', views.delete, name='delete'),
 ]
